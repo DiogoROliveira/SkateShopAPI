4976edf4cc6bdbf47096782a917c3cd68bae1fc8
tiago <100705276+Tiago02p@users.noreply.github.com>
2024-12-04 23:02:20 +0000

    ERP Client integration

--- src/config/jasminConfig.js
+++
@@ -1,5 +0,0 @@
-const jasminBaseURL = process.env.JASMIN_BASE_URL || "https://my.jasminsoftware.com/api";
-const jasminUser = process.env.JASMIN_ACCOUNT;
-const jasminSub = process.env.JASMIN_SUBSCRIPTION;
-
-module.exports = { jasminBaseURL, jasminUser, jasminSub };
